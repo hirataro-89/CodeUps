5cae6ae107b569e098ecda374e14bebc8f5c607f
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-07-01 14:51:58 +0900

    Add: section-campaign

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,34 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 });
 
 /* swiper mainView */
-const swiper = new Swiper('.mainView-swiper', {
+const mainviewSwiper = new Swiper('.mainViewSwiper', {
     loop: true,
     autoplay: {
         delay: 4000,
     },
     effect: 'fade',
     speed: 900
+});
+
+/* swiper campaig */
+const campaignSwiper = new Swiper('.campaignSwiper__slide', {
+    loop: true,
+    speed: 900,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    spaceBetween: 24,
+    slidesPerView: 1.2,
+    /* pc時 */
+    breakpoints: {
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 40,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        }
+    }
 });
