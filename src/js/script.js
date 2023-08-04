b48eee76a632c61cf6cc9a5e83bbb8ccc68ca589
yutaro hirabayashi <hirataro.yu@gmail.com>
2023-08-05 06:41:52 +0900

    Fix: デザイン修正

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded',function(){
             if(hashIDName){
                 //タブ設定
                 $('.js-tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-                    var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
+                    var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得
                     if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
                         var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
                         $('.js-tab li').removeClass("is-active"); //タブ内のliについているactiveクラスを取り除き
